012abbb02344e94c3ad9edd2dca95b2b9cd84283
David Collett <63430066+robotnumber3@users.noreply.github.com>
2025-01-29 12:18:50 -0800

    Clean up print statements

--- my_program1.py
+++ my_program1.py
@@ -7,8 +7,8 @@ b = 4.915
 c = 6.713
 
 print(f'{a = }, {b = }, {c = }')
-print(a*b/c)
+print(f'{a*b/c = }')
 
-print(f'{add(a,b) = }, {subtract(a,b) = }, {multiply(a,b) = }, {divide(a,b) = }')
+print(f'{add(a,b) = }\n{subtract(a,b) = }\n{multiply(a,b) = }\n{divide(a,b) = }')
 
 
