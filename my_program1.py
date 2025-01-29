fc02dc47e8f35e4e6d80f6973eb96807477c3b0d
David Collett <63430066+robotnumber3@users.noreply.github.com>
2025-01-29 12:15:29 -0800

    Update main to use utils

--- my_program1.py
+++ my_program1.py
@@ -1,9 +1,14 @@
 # my_program1.py
 
+from utils import add, subtract, multiply, divide
+
 a = 3
 b = 4
 c = 6.713
 
 print(f'{a = }, {b = }, {c = }')
 print(a*b/c)
+
+print(f'{add(a,b) = }, {subtract(a,b) = }, {multiply(a,b) = }, {divide(a,b) = }')
+
 
