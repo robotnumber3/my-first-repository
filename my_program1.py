526129d74969a83fe3622eb9eb81dd230139f872
David Collett <63430066+robotnumber3@users.noreply.github.com>
2025-01-30 10:41:27 -0800

    Update print statement

--- my_program1.py
+++ my_program1.py
@@ -1,6 +1,6 @@
 # my_program1.py
 
-from utils import add, subtract, multiply, divide
+from utils import add, subtract, multiply, divide, power
 
 a = 3.27
 b = 4.915
@@ -9,6 +9,4 @@ c = 6.713
 print(f'{a = }, {b = }, {c = }')
 print(f'{a*b/c = }')
 
-print(f'{add(a,b) = }\n{subtract(a,b) = }\n{multiply(a,b) = }\n{divide(a,b) = }')
-
-
+print(f'{add(a,b) = }\n{subtract(a,b) = }\n{multiply(a,b) = }\n{divide(a,b) = }\n{power(a,b) = }')
